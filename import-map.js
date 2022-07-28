fcc5a61f8ff0df5413e94cd74396cb550b1a0832
Pedro Sousa <sketchpunk@gmail.com>
2022-07-28 10:34:55 -0400

    add localhost check in import map

--- import-map.js
+++ import-map.js
@@ -1,11 +1,15 @@
 // in the future can prob do : <script type="importmap" src="/import-map.json"></script>
+const prepend = ( document.location.hostname.indexOf( 'localhost' ) === -1 )? '/ballerxr' : '';
+
 document.body.appendChild(Object.assign(document.createElement('script'), {
 type		: 'importmap',
 innerHTML	: `
     {"imports":{
-        "three"             : "/thirdparty/three.module.min.js",
-        "OrbitControls"	    : "/thirdparty/OrbitControls.js",
-        "gltfParser"        : "/thirdparty/gltf2parser.es.js",
-        "gl-matrix"         : "/thirdparty/gl-matrix/index.js"
+        "three"             : "${prepend}/thirdparty/three.module.min.js",
+        "OrbitControls"	    : "${prepend}/thirdparty/OrbitControls.js",
+        "gltfParser"        : "${prepend}/thirdparty/gltf2parser.es.js",
+        "gl-matrix"         : "${prepend}/thirdparty/gl-matrix/index.js"
     }}
 `}));
+
+console.log( document.location.hostname.indexOf( 'localhost' ) );
